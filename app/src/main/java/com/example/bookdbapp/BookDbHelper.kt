571f3f5e5905af259020b0e3307fcf708c11c1f1
aaa <b11007007@gapps.knu.edu.tw>
2024-06-05 15:56:18 +0800

    Update

---
+++ app/src/main/java/com/example/bookdbapp/BookDbHelper.kt
@@ -0,0 +1,198 @@
+package com.example.bookdbapp
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
+class BookDbHelper private constructor(val context: Context) :
+    SQLiteOpenHelper(context, DB_FILE, null, 1) {
+
+        companion object {
+            private const val DB_FILE = "books.db"
+            private const val BOOK_TABLE = "books"
+            private const val AUTHOR_TABLE = "authors"
+            private const val PUBLISHER_TABLE = "publishers"
+            private const val ID = "id"
+            private const val TITLE = "title"
+            private const val AUTHOR_ID = "author_id"
+            private const val PUBLISHER_ID = "publisher_id"
+            private const val AUTHOR_NAME = "author_name"
+            private const val PUBLISHER_NAME = "publisher_name"
+
+            private var bookDbHelper: BookDbHelper? = null
+
+            fun init(context: Context) {
+                if (bookDbHelper == null)
+                    bookDbHelper = BookDbHelper(context)
+            }
+
+            fun getInstance(): BookDbHelper? {
+                return bookDbHelper
+            }
+        }
+    override fun onCreate(db: SQLiteDatabase?) {
+        val sqlAuthorTable = "create table $AUTHOR_TABLE ($ID integer primary key, " +
+                "$AUTHOR_NAME nvarchar(20));"
+        val sqlPublisherTable = "create table $PUBLISHER_TABLE ($ID integer primary key, " +
+                "$PUBLISHER_NAME nvarchar(50));"
+        val sqlBookTable = "create table $BOOK_TABLE ($ID integer primary key, " +
+                "$TITLE nvarchar(30), $AUTHOR_ID integer, $PUBLISHER_ID integer, " +
+                "foreign key ($AUTHOR_ID) references $AUTHOR_TABLE($ID), " +
+                "foreign key ($PUBLISHER_ID) references $PUBLISHER_TABLE($ID));"
+
+        db?.execSQL(sqlAuthorTable)
+        db?.execSQL(sqlPublisherTable)
+        db?.execSQL(sqlBookTable)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        if (oldVersion != newVersion) {
+            db?.execSQL("drop table if exists $BOOK_TABLE")
+            onCreate(db)
+        }
+    }
+
+    fun addBook(title: String, author: String, publisher: String) {
+        var c = readableDatabase.query(AUTHOR_TABLE, arrayOf(ID), "$AUTHOR_NAME = ?",
+            arrayOf(author), null, null, null)
+
+        if (c.count == 0) return
+        c.moveToFirst()
+        val authorId = c.getInt(0);
+
+        c = readableDatabase.query(
+            PUBLISHER_TABLE, arrayOf(ID), "$PUBLISHER_NAME = ?",
+            arrayOf(publisher), null, null, null)
+
+        if (c.count == 0) return
+        c.moveToFirst()
+        val publisherId = c.getInt(0);
+
+        val cv = ContentValues()
+        cv.put(TITLE, title)
+        cv.put(AUTHOR_ID, authorId)
+        cv.put(PUBLISHER_ID, publisherId)
+
+        writableDatabase.insert(BOOK_TABLE, null, cv)
+        writableDatabase.close()
+    }
+
+    fun printAllBooks() {
+        val c = readableDatabase.query(BOOK_TABLE, arrayOf(TITLE, AUTHOR_ID, PUBLISHER_ID),
+            null, null, null, null, null)
+
+        if (c.count != 0) {
+            c.moveToFirst()
+            do {
+                Toast.makeText(context, "Book: ${c.getString(0)}, " +
+                        "${c.getString(1)}, ${c.getString(2)}", Toast.LENGTH_LONG).show()
+            } while (c.moveToNext())
+        }
+    }
+
+    fun getAllBooks(): ArrayList<Book> {
+        val c = readableDatabase.rawQuery("select $BOOK_TABLE.$ID, $BOOK_TABLE.$TITLE, $AUTHOR_TABLE.$AUTHOR_NAME, $PUBLISHER_TABLE.$PUBLISHER_NAME " +
+        "from $BOOK_TABLE inner join $AUTHOR_TABLE on $BOOK_TABLE.$AUTHOR_ID=$AUTHOR_TABLE.$ID " +
+        "inner join $PUBLISHER_TABLE on $BOOK_TABLE.$PUBLISHER_ID=$PUBLISHER_TABLE.$ID", null)
+
+        val books = arrayListOf<Book>()
+
+        if (c.count != 0) {
+            c.moveToFirst()
+            do {
+                val book = Book(
+                    c.getInt(0),
+                    c.getString(1),
+                    c.getString(2),
+                    c.getString(3)
+                    )
+                books.add(book)
+            } while (c.moveToNext())
+        }
+
+        return books
+    }
+
+    fun delBook(id: Int) {
+        writableDatabase.delete(BOOK_TABLE, "$ID = ?", arrayOf(id.toString()))
+        writableDatabase.close()
+    }
+
+    fun updateBook(book: Book) {
+        val cv = ContentValues()
+        cv.put(TITLE, book.title)
+        cv.put(AUTHOR_ID, book.author)
+        cv.put(PUBLISHER_ID, book.publisher)
+
+        writableDatabase.update(BOOK_TABLE, cv, "$ID = ?", arrayOf(book.id.toString()))
+        writableDatabase.close()
+    }
+
+    fun queryBooks(book: Book): ArrayList<Book> {
+        var conditionExists = false
+        var selection = ""
+
+        if (book.title.isNotEmpty()) {
+            selection = "$TITLE = '${book.title}'"
+            conditionExists = true
+        }
+
+        if (book.author.isNotEmpty()) {
+            if (!conditionExists) {
+                selection = "$AUTHOR_ID = '${book.author}'"
+                conditionExists = true
+            } else {
+                selection = "$selection and $AUTHOR_ID = '${book.author}'"
+            }
+        }
+
+        if (book.publisher.isNotEmpty()) {
+            if (!conditionExists) {
+                selection = "$PUBLISHER_ID = '${book.publisher}'"
+                conditionExists = true
+            } else {
+                selection = "$selection and $PUBLISHER_ID = '${book.publisher}'"
+            }
+        }
+
+        val books = arrayListOf<Book>()
+
+        if (!conditionExists) return books
+
+        val c = readableDatabase.query(BOOK_TABLE, arrayOf(ID, TITLE, AUTHOR_ID, PUBLISHER_ID),
+            selection, null, null, null, null)
+
+        if (c.count != 0) {
+            c.moveToFirst()
+            do {
+                val book = Book(
+                    c.getInt(0),
+                    c.getString(1),
+                    c.getString(2),
+                    c.getString(3)
+                )
+                books.add(book)
+            } while (c.moveToNext())
+        }
+
+        return books
+    }
+
+    fun addAuthor(name: String) {
+        val cv = ContentValues()
+        cv.put(AUTHOR_NAME, name)
+
+        writableDatabase.insert(AUTHOR_TABLE, null, cv)
+        writableDatabase.close()
+    }
+
+    fun addPublisher(name: String) {
+        val cv = ContentValues()
+        cv.put(PUBLISHER_NAME, name)
+
+        writableDatabase.insert(PUBLISHER_TABLE, null, cv)
+        writableDatabase.close()
+    }
+}
---
+++ app/src/main/java/com/example/bookdbapp/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.bookdbapp
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookdbapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var dataBinding: ActivityMainBinding
+
+    private lateinit var bookList: ArrayList<Book>
+    private lateinit var bookRecyclerViewAdapter: BookRecyclerViewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        bookList = arrayListOf()
+        bookRecyclerViewAdapter = BookRecyclerViewAdapter(bookList)
+        dataBinding.rvBookList.adapter = bookRecyclerViewAdapter
+        dataBinding.rvBookList.layoutManager = LinearLayoutManager(this)
+
+        BookDbHelper.init(this)
+        val dbHelper = BookDbHelper.getInstance()
+
+        dbHelper?.addAuthor("aaa")
+        dbHelper?.addAuthor("bbb")
+        dbHelper?.addPublisher("111")
+        dbHelper?.addPublisher("222")
+
+        dataBinding.btAdd.setOnClickListener {
+            dbHelper?.addBook(
+                dataBinding.edTitle.text.toString(),
+                dataBinding.edAuthor.text.toString(),
+                dataBinding.edPublisher.text.toString()
+            )
+        }
+
+        dataBinding.btShow.setOnClickListener {
+            bookList.clear()
+            val books = dbHelper?.getAllBooks()
+            books?.let {
+                bookList.addAll(books)
+                bookRecyclerViewAdapter.notifyDataSetChanged()
+            }
+        }
+
+        dataBinding.btQuery.setOnClickListener {
+            bookList.clear()
+
+            val book = Book(
+                -1,
+                dataBinding.edTitle.text.toString(),
+                dataBinding.edAuthor.text.toString(),
+                dataBinding.edPublisher.text.toString()
+            )
+            val books = dbHelper?.queryBooks(book)
+            books?.let {
+                bookList.addAll(books)
+                bookRecyclerViewAdapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    private class BookRecyclerViewAdapter(private val books: ArrayList<Book>) : RecyclerView.Adapter<BookRecyclerViewAdapter.ViewHolder>() {
+        data class ViewHolder(val bookView: View) : RecyclerView.ViewHolder(bookView) {
+            val tvTitle = bookView.findViewById<TextView>(R.id.tvTitle)
+            val tvAuthor = bookView.findViewById<TextView>(R.id.tvAuthor)
+            val tvPublisher = bookView.findViewById<TextView>(R.id.tvPublisher)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.book_item, parent, false)
+            return ViewHolder(v)
+        }
+
+        override fun getItemCount(): Int {
+            return books.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val book = books[position]
+            holder.tvTitle.text = book.title
+            holder.tvAuthor.text = book.author
+            holder.tvPublisher.text = book.publisher
+
+            holder.bookView.setOnLongClickListener {
+                val dlg = BottomSheetDialog(it.context, R.style.BottomSheetDialog)
+                dlg.setContentView(R.layout.dlg_del_update)
+                val btDel = dlg.findViewById<Button>(R.id.btDel)
+                val btUpdate = dlg.findViewById<Button>(R.id.btUpdate)
+
+                btDel?.setOnClickListener {
+                    BookDbHelper.getInstance()?.delBook(book.id)
+                    books.removeAt(position)
+                    notifyDataSetChanged()
+                    dlg.dismiss()
+                }
+
+                btUpdate?.setOnClickListener {
+                    val updateDlg = Dialog(it.context)
+                    updateDlg.setContentView(R.layout.dlg_update)
+                    updateDlg.setCancelable(false)
+                    updateDlg.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                    val edDlgTitle = updateDlg.findViewById<EditText>(R.id.edDlgTitle)
+                    val edDlgAuthor = updateDlg.findViewById<EditText>(R.id.edDlgAuthor)
+                    val edDlgPublisher = updateDlg.findViewById<EditText>(R.id.edDlgPublisher)
+                    val btOk = updateDlg.findViewById<Button>(R.id.btOk)
+                    val btCancel = updateDlg.findViewById<Button>(R.id.btCancel)
+
+                    edDlgTitle.setText(book.title)
+                    edDlgAuthor.setText(book.author)
+                    edDlgPublisher.setText(book.publisher)
+
+                    btOk?.setOnClickListener {
+                        val newData = Book(
+                            book.id,
+                            edDlgTitle.text.toString(),
+                            edDlgAuthor.text.toString(),
+                            edDlgPublisher.text.toString())
+
+                        BookDbHelper.getInstance()?.updateBook(newData)
+                        books.removeAt(position)
+                        books.add(position, newData)
+                        notifyDataSetChanged()
+                        updateDlg.dismiss()
+                        dlg.dismiss()
+                    }
+
+                    btCancel?.setOnClickListener {
+                        updateDlg.dismiss()
+                        dlg.dismiss()
+                    }
+                    
+                    updateDlg.show()
+                }
+
+                dlg.show()
+                true
+            }
+        }
+    }
+}
